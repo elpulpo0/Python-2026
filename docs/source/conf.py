c82f9bbe1530cd7da2c4373814123c00df7c0b58
elpulpo0 <chris.elpulpo@gmail.com>
2026-01-12 16:17:53 +0100

    feat: update conf docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Python 2026'
-copyright = '2026, Chris'
-author = 'Chris'
-release = '0'
+project = "Python 2026"
+copyright = "2026, Chris"
+author = "Chris"
+release = "0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",  # Pour extraire la doc de ton code
+    "sphinx.ext.napoleon",  # Pour supporter les docstrings style Google/NumPy
+    "sphinx.ext.mathjax",  # Pour latex
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
